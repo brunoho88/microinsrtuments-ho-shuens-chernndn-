7606b127d2ed9b39ce31e128a93622d5d1cbf5f7
brunoho88 <114266743+brunoho88@users.noreply.github.com>
2022-09-24 07:40:14 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onPinPressed(TouchPin.P0, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+})
+input.onPinPressed(TouchPin.P2, function () {
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+})
+input.onPinPressed(TouchPin.P1, function () {
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+})
+basic.forever(function () {
+	
+})
